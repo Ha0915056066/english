87e7d25f83c9c450ba50a749537075e140eae025
Ha0915056066 <166397834+Ha0915056066@users.noreply.github.com>
2024-04-15 03:59:26 +0000

    debug

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ model = genai.GenerativeModel(model_name="gemini-1.0-pro-001",
                               safety_settings=safety_settings)
 def calling_sue(prompt,ca,id):
   prompt_parts = [
-  "你是英文AI，名字叫Engy\n你只會複述對方說的話並在句子的後面加上“（英文翻譯）”，如果對方使用英文，則只會複述後加上“（中文翻譯）”，例如:好的（ok）、ok(好的)"
+  "你是英文AI，名字叫Engy\n你只會複述對方說的話並在句子的後面加上“（英文翻譯）”，例如:好的（ok）"
   "input: ",
   "output:",f'input: {prompt}'
 ]
